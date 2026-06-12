e4b5bd9918d1d2404253d6617459bf047c1f4cb8
Piyanka-murali <priyankaheman225@gmail.com>
2026-06-12 09:06:24 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,25 @@
+package com.activity;
+
+public class Activity2 {
+
+	public static void main(String[] args) {		
+
+		int[] arr = {10, 77, 10, 54, -11, 10};
+
+
+		int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 10) {
+                sum = sum + 10;
+            }
+        }
+
+        if (sum == 30) {
+            System.out.println("Result: true");
+        } else {
+            System.out.println("Result: false");
+        }
+    }
+
+	}
